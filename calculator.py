2f446b35786ad56c47fff6107cc6a02bd2f0437a
Hackbright Student <info@hackbrightacademy.com>
2015-01-14 18:15:11 -0800

    Now identifies operation and converts input into integer. About to add arithmetic.py calls within function

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ calculator.py
 Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
-
+import arithmetic2
 
 def main():
     # repeat forever:
@@ -19,6 +19,20 @@ def main():
         if tokens[0] == "q":
             return
     #    otherwise decide which math function to call based on the tokens we read
+        else:
+            if tokens[0] in ["+" , "-" , "*" , "/" , "square" , "cube" , "pow" , "mod"]:
+                input_int1 = int(tokens[1])
+                input_int2 = int(tokens[2])
+                    if tokens[0] ==
+                #print input_int1
+                #print input_int2 
+                #[0] == ("+" , "-" , "*" , "/" , "square" , "cube" , "pow" , "mod"):
+            #input_int1 = int(tokens[1])
+            #input_int2 = int(tokesn[2])
+            # answer = arithmetic2.add(int(tokens[1] , tokens[2])
+            # print answer
+            #print(input_int1 , input_int2)
+       
 
 
 if __name__ == '__main__':
