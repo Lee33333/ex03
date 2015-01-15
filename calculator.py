3d04ba6bfcab8d9ecba2747d04debbfc9f678757
Hackbright Student <info@hackbrightacademy.com>
2015-01-14 18:39:06 -0800

    Calculator now takes input and correctly calls arithmetic2 functions.

--- calculator.py
+++ calculator.py
@@ -19,13 +19,43 @@ def main():
         if tokens[0] == "q":
             return
     #    otherwise decide which math function to call based on the tokens we read
+        elif tokens[0] in ["+" , "-" , "*" , "/" , "pow" , "mod"]:
+            input_int1 = int(tokens[1])
+            input_int2 = int(tokens[2])
+            if tokens[0] == "+":
+                answer = arithmetic2.add(input_int1 , input_int2)
+                print answer
+            if tokens[0] == "-":
+                answer = arithmetic2.subtract(input_int1 , input_int2)
+                print answer
+            if tokens[0] == "*":
+                answer = arithmetic2.multiply(input_int1 , input_int2)
+                print answer
+            if tokens[0] == "/":
+                answer = arithmetic2.divide(input_int1 , input_int2)
+                print answer
+            if tokens[0] == "square":
+                answer = arithmetic2.square(input_int1)
+                print answer
+            if tokens[0] == "cube":
+                answer = arithmetic2.cube(input_int1)
+                print answer
+            if tokens[0] == "pow":
+                answer = arithmetic2.power(input_int1 , input_int2)
+                print answer
+            if tokens[0] == "mod":
+                answer = arithmetic2.mod(input_int1 , input_int2)
+                print answer
         else:
-            if tokens[0] in ["+" , "-" , "*" , "/" , "square" , "cube" , "pow" , "mod"]:
+            if tokens[0] in ["square" , "cube"]:
                 input_int1 = int(tokens[1])
-                input_int2 = int(tokens[2])
-                    #if tokens[0] ==
-                print input_int1
-                print input_int2 
+                if tokens[0] == "square":
+                    answer = arithmetic2.square(input_int1)
+                    print answer
+                if tokens[0] == "cube":
+                    answer = arithmetic2.cube(input_int1)
+                #print input_int1
+                #print input_int2 
                 #[0] == ("+" , "-" , "*" , "/" , "square" , "cube" , "pow" , "mod"):
             #input_int1 = int(tokens[1])
             #input_int2 = int(tokesn[2])
