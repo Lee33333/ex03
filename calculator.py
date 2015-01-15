dc86bfc47f7b6c010d80f8232c447ef55b4d7e06
Hackbright Student <info@hackbrightacademy.com>
2015-01-14 17:46:22 -0800

    Working calculator before importing arithmetic.py

--- calculator.py
+++ calculator.py
@@ -7,8 +7,18 @@ calculator program yourself in this file.
 
 
 def main():
-	# Your code goes here
-	pass
+    # repeat forever:
+    while True:
+        #    read input
+        user_input = raw_input("Enter your operation > ")
+
+    #    tokenize input
+        tokens = user_input.split(" ")
+        #print tokens
+    #    if the first token is 'q', quit
+        if tokens[0] == "q":
+            return
+    #    otherwise decide which math function to call based on the tokens we read
 
 
 if __name__ == '__main__':
